0213e940e71ceac038f63e5268e5f245bf1b32c0
ganli <gl09025@gmail.com>
2018-03-14 15:47:05 +0800

    完成todolist的基本功能

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ var app = new Vue({
               done: false
           })
           this.newTodo = ''
+          this.saveOrUpdateTodos()
         },
         removeTodo: function (todo) {
             let index = this.todoList.indexOf(todo)
             this.todoList.splice(index,1)
+            this.saveOrUpdateTodos()
         },
         signUp: function () {
             let user = new AV.User();
@@ -50,6 +52,7 @@ var app = new Vue({
             AV.User.logIn(this.formData.username, this.formData.password).then( (loginedUser) => {
                 console.log(loginedUser);
                 this.currentUser = this.getCurrentUser()
+                this.fetchTodos()
               }, function (error) {
                   alert("登录失败")
               });
@@ -70,17 +73,64 @@ var app = new Vue({
             // 现在的 currentUser 是 null 了
             this.currentUser = null
             window.location.reload()
+        },
+        saveTodos: function () {
+            let dateString = JSON.stringify(this.todoList)
+            let AVTodos = AV.Object.extend('AllTodos')
+            let avTools = new AVTodos
+            let acl = new AV.ACL() 
+            acl.setReadAccess(AV.User.current(),true)
+            acl.setWriteAccess(AV.User.current(),true)
+            avTools.set('content',dateString)
+            avTools.setACL(acl)
+            avTools.save().then( (todo) => {
+                this.todoList.id = todo.id
+                console.log('保存成功')
+            },function (error) {
+                console.error('保存失败')
+            })
+        },
+        updateTodos: function () {
+            let dateString = JSON.stringify(this.todoList)
+            let avTodos = AV.Object.createWithoutData('AllTodos',this.todoList.id)
+            avTodos.set('content',dateString)
+            avTodos.save().then(()=>{
+                console.log('更新成功')
+            })
+        },
+        saveOrUpdateTodos: function () {
+            if(this.todoList.id) {
+                this.updateTodos()
+            }else {
+                this.saveTodos()
+            }
+        },
+        fetchTodos: function () {
+            if(this.currentUser){
+                var query = new AV.Query('AllTodos');
+                query.find()
+                    .then((todos) => {
+                        let avAllTodos = todos[0]
+                        let id = avAllTodos.id
+                        this.todoList = JSON.parse(avAllTodos.attributes.content)
+                        this.todoList.id = id
+                    }, function (error) {
+                        console.error(error)
+                    })
+                }
         }
     },
     created: function () {
-        window.onbeforeunload = () => {
-            let dateString = JSON.stringify(this.todoList)
-            window.localStorage.setItem('myTodos',dateString)
-        }
+        // window.onbeforeunload = () => {
+        //     let dateString = JSON.stringify(this.todoList)
+        //     window.localStorage.setItem('myTodos',dateString)
+        // }
 
-        let oldDateString = window.localStorage.getItem('myTodos')
-        let oldDate = JSON.parse(oldDateString)
-        this.todoList = oldDate || []
+
+        // let oldDateString = window.localStorage.getItem('myTodos')
+        // let oldDate = JSON.parse(oldDateString)
+        // this.todoList = oldDate || []
         this.currentUser = this.getCurrentUser()
+        
     }
   })
